b86e4f5dd5eb451271e87498bc256eaa3c4dfc2b
Khoa Nguyen Toan <57111782+nikola-ng@users.noreply.github.com>
2020-04-29 19:12:11 +0700

    Add files via upload

---
+++ Homework_NTKhoa/tinhtichphan.hpp
@@ -0,0 +1,16 @@
+//
+//  tinhtichphan.hpp
+//  tinhtichphansinx
+//
+//  Created by Khoa Nguyen on 4/26/20.
+//  Copyright © 2020 Khoa Nguyen. All rights reserved.
+//
+
+#ifndef tinhtichphan_hpp
+#define tinhtichphan_hpp
+
+#include <stdio.h>
+
+void tinhtichphan(double (*f)(double), double a, double b, int n);
+
+#endif /* tinhtichphan_hpp */
