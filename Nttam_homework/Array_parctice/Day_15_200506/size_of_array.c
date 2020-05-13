22f61ce7109d4958a3f20dd0fb433008d8a9b5b8
Nguyen Thien Tam <thientam.2695@gmail.com>
2020-05-13 11:05:11 +0700

    modify

--- Nttam_homework/Array_parctice/Day_15_200506/size_of_array.c
+++ Nttam_homework/Array_parctice/Day_15_200506/size_of_array.c
@@ -7,9 +7,10 @@ void printLenght(int intArray[]);
 
 void main()
 {
+	char a[] = { 0,1,2,3,4};;
 	int array[] = { 0,1,2,3,4,5,6 };
 	printf("size of array: %d\r\n", (int)sizeof(array));
-	printSizeOf(array);
+	printSizeOf(a);
 
 	printf("Length of array: %d\r\n", (int)(sizeof(array) / sizeof(array[0])));
 	printLenght(array);
@@ -24,5 +25,7 @@ void printSizeOf(int intArray[])
 
 void printLenght(int intArray[])
 {
+	printf("lenght of parameter 1: %d\r\n", (int)sizeof(intArray));
+	printf("lenght of parameter 2: %d\r\n", (int)sizeof(intArray[0]));
 	printf("lenght of parameter: %d\r\n", (int)(sizeof(intArray) / sizeof(intArray[0])));
 }
